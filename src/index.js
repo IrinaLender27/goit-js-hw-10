cd7755e9ed39fbf6721fa4bec7f2ebd323eee328
IrinaLender27 <irina.lender27@gmail.com>
2023-12-07 00:01:47 +0200

    cat update

--- src/index.js
+++ src/index.js
@@ -1,55 +1,54 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
-
+import SlimSelect from 'slim-select';
+import Notiflix, { Notify } from 'notiflix';
+import 'slim-select/dist/slimselect.css';
+// змінні
 const container = document.querySelector('.cat-info');
-const select = document.querySelector('.breed-select');
-fetchBreeds();
+const selectcat = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
 
-function createMarkup(arr) {
-  return arr
-    .map(
-      ({ name, description, image, temperament }) =>
-        `<li>
-        <img src="
-"https://cdn2.thecatapi.com/images/$" alt="${name}" />
-        <h2>${name}</h2>
-        <p>${description}</p>
-        <p>${temperament}</p>
-      </li>`
-    )
-    .join('');
+fetchBreeds()
+  .then(data => {
+    loader.classList.replace('loader', 'is-hidden');
+
+    let createMarckSelect = data.map(({ name, id }) => {
+      return `<option value ='${id}'> ${name} </option>`;
+    });
+    selectcat.insertAdjacentHTML('beforeend', createMarckSelect);
+    new SlimSelect({
+      select: selectcat,
+    });
+  })
+  .catch(onError);
+
+// додавання прослуховувача на подію
+selectcat.addEventListener('change', onChangeBreed);
+
+function onChangeBreed(event) {
+  loader.classList.replace('is-hidden', 'loader');
+  selectcat.classList.add('is-hidden');
+  container.classList.add('is-hidden');
+  let breedId = event.currentTarget.value;
+  console.log(breedId);
+  fetchCatByBreed(breedId)
+    .then(data => {
+      loader.classList.replace('loader', 'is-hidden');
+      selectcat.classList.remove('is-hidden');
+      const { url, breeds } = data[0];
+      container.innerHTML = `<img src="${url}" alt="${breeds[0].name}" width="400"/>
+        <div class="cat-info">
+        <h2>${breeds[0].name}</h2>
+        <p>${breeds[0].description}</p>
+        <p>${breeds[0].temperament}</p>
+        </div>`;
+      container.classList.remove('is-hidden');
+    })
+    .catch(onError);
 }
-//     return arr
-//     .map(
-//       ({
-//         date,
-//         day: {
-//           avgtemp_c,
-//           condition: { icon, text },
-//         },
-//       }) =>
-//         `<li>
-//         <img src="${icon}" alt="${text}" />
-//         <p>${text}</p>
-//         <h2>${date}</h2>
-//         <h3>${avgtemp_c}</h3>
-//       </li>`
-//     )
-//     .join('');
-// }
-// // return arr
-//   .map(
-//     ({
-//       date,
-//       day: {
-//         avgtemp_c,
-//         condition: { icon, text },
-//       },
-//     }) =>
-//       `<li>
-//         <img src="${icon}" alt="${text}" />
-//         <p>${text}</p>
-//         <h2>${date}</h2>
-//         <h3>${avgtemp_c}</h3>
-//       </li>`
-//   )
-//   .join('');
+
+function onError() {
+  selectcat.classList.remove('is-hidden');
+  loader.classList.replace('loader', 'is-hidden');
+  Notify.failure('Oops! Something went wrong! Try reloading the page!');
+}
